2ab4e2902b560d1c5b0ec5f98611d59c54f4cee1
Gavin <waltersg298@gmail.com>
2022-10-09 16:56:37 -0600

    added button toggle

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('testing');
+// Read "Update to API" portion if running into issues. 
+
+// console.log('testing');
+
+// Get quotes from API 
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+
+    try {
+
+    } 
+    catch (error) {
+        // Catch error here 
+    } 
+}
