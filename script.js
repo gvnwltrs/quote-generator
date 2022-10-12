22f2917dc88bc870389088270383d078a6d8f924
Gavin <waltersg298@gmail.com>
2022-10-11 18:58:30 -0600

    added functionality to buttons for quote and twitter posting

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show new quote 
 function newQuote() {
     // Pick a random quote from apiQuotes array 
-    if (apiQuotes != null) {
-        const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-        console.log(quote);
-        return true; 
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    
+    // Check quote length to determine styling 
+    if (quote.text.length > 50) {
+        quoteText.classList.add('long-quote'); 
     }
-    return false;
+    else {
+        quoteText.classList.remove('long-quote'); 
+    }
+    quoteText.textContent = quote.text; 
+    
+    // Check if author filed is blank and replace with 'Unknown'
+    if (quote.author == null) {
+        authorText.textContent = "Unknown"; 
+    }
+    else {
+        authorText.textContent = quote.author; 
+    }
+    
 }
 
 // Get quotes from API 
@@ -18,7 +37,7 @@ async function getQuotes() {
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(typeof apiQuotes);
+        // console.log(apiQuotes);
         newQuote();
     } 
     catch (error) {
@@ -26,11 +45,21 @@ async function getQuotes() {
     } 
 }
 
+// Tweet Quote 
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank'); 
+}
+
+// Event Listeners 
+newQuoteBtn.addEventListener('click', newQuote); 
+twitterBtn.addEventListener('click', tweetQuote);
+
 // On load 
 getQuotes();
 
-if (typeof module === 'object') {
-    module.exports = { apiQuotes, newQuote, getQuotes }; 
-}
 
-// export {apiQuotes}
+// Tests 
+if (typeof module === 'object') {
+    module.exports = { apiQuotes, newQuote, getQuotes, quoteContainer}; 
+}
